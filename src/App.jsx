38f7b026d50b125c123eca501c4af39fb95124fe
Ana Clara Bergamini Delgado <ana4ew.br@gmail.com>
2023-12-20 23:29:12 -0300

    Criando pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './styles/App.css'
 import Home from './pages/home.jsx'
+import MonteSuaPizza from './pages/monteSuaPizza.jsx'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/MonteSuaPizza" element={<MonteSuaPizza />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/monteSuaPizza.jsx
@@ -0,0 +1,7 @@
+function monteSuaPizza() {
+    return (
+        <>
+            
+        </>
+    )
+}
