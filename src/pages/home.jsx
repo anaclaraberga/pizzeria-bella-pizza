52a6f3af386887a3be5a58ebf2f469088afa7866
Ana Clara Bergamini Delgado <ana4ew.br@gmail.com>
2023-12-21 22:02:29 -0300

    Atualização

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,12 @@
+import Background from '../assets/background.svg' 
+
 function Home() {
 
     return (
         <>
+            <div class="background">
+                <img src={Background} className="background" alt="background"/>
+            </div>
             <h1>hi</h1>
         </>
     )
